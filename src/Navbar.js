c0aed0d466d99bc2e455b1b39b8ff82f4df06cce
clvalada <clvalada@gmail.com>
2023-11-30 10:54:32 -0500

    navbar

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1>Chris Valada</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/about">About Me</a>
+                <a href="/portfolio">Portfolio</a>
+                <a href="/contact">Contact Me</a>
+                <a href="/resume">Resume</a>
+            </div> 
+        </nav>
+    );
+}
+ 
+export default Navbar;
