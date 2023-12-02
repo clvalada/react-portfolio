2017c5d2abd291519a0f42a2200eb20cf3ebf01a
clvalada <clvalada@gmail.com>
2023-12-02 15:36:00 -0500

    updated dependencies. working on navigation.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-/* App.js*/
+// App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
-import Projects from './Projects';
-/*import About from './About';
-import Contact from './Contact';
+import About from './About';
 import Portfolio from './Portfolio';
-import Resume from './Resume';*/
+import Contact from './Contact';
+import Resume from './Resume';
 import Footer from './Footer';
 
-
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Navbar  />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/resume" component={Resume} />
+          </Switch>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,9 +1,36 @@
-const Portfolio = () => {
-    return (  
-        <div className="portfolio">
-            <h2>Portfolio</h2>
-        </div>
-    );
-}
- 
-export default Portfolio;
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import projectData from './ProjectData';
+
+const ProjectCard = ({ image, description, githubLink, deployedLink }) => {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image} alt="Project" />
+      <Card.Body>
+        <Card.Title>{description}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary" href={githubLink} target="_blank" rel="noopener noreferrer">
+          GitHub
+        </Button>
+        <Button variant="success" href={deployedLink} target="_blank" rel="noopener noreferrer">
+          Deployed Project
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+const Projects = ({ projects }) => {
+  return (
+    <div className="projects-container">
+      {projects.map((project, index) => (
+        <ProjectCard key={index} {...project} />
+      ))}
+    </div>
+  );
+};
+
+export default Projects;
