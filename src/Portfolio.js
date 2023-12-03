6f6fbf5197ea55a3c7698e5cc18a643d1e85e4ac
clvalada <clvalada@gmail.com>
2023-12-02 21:51:19 -0500

    css styling

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -5,20 +5,24 @@ import projectData from './ProjectData';
 import PropTypes from 'prop-types';
 
 
+
+
 function ProjectCard({ image, description, githubLink, deployedLink }) {
+  console.log('Image Path:, image');
   return (
     <div className="custom-card">
-      {/* Your custom card styling goes here */}
       <img src={image} alt="Project" />
-      <div className="card-content">
-        <h3>{description}</h3>
-        {/* Other card content */}
-        <a href={githubLink} target="_blank" rel="noopener noreferrer">
-          GitHub
-        </a>
-        <a href={deployedLink} target="_blank" rel="noopener noreferrer">
-          Deployed Project
-        </a>
+      <div>
+        <h4>{description}</h4>
+        <hr></hr>
+        <div className="container">
+            <a href={githubLink} target="_blank" rel="noopener noreferrer">
+            GitHub Repo 
+            </a>
+            <a href={deployedLink} target="_blank" rel="noopener noreferrer">
+            Deployed Project
+          </a>
+        </div>
       </div>
     </div>
   );
@@ -36,6 +40,7 @@ function Portfolio() {
   return (
     <div className="portfolio">
       <h2>Portfolio</h2>
+    
       <div className="projects-container">
         {projectData.map((project, index) => (
           <ProjectCard key={index} {...project} />
--- src/ProjectData.js
+++ src/ProjectData.js
@@ -1,36 +1,36 @@
 const projectData = [
     {
-      image: 'src/images/bdayapp.jpg',
+      image: './src/images/bdayapp.jpg',
       description: 'The Birthday App is a web app that provides users with historical events that happened on their birthday. It utilizes HTML, Tailwind CSS, JavaScript, and two 3rd party APIs.',
       githubLink: 'https://github.com/lindseycarlson23/birthday-app',
       deployedLink: 'https://lindseycarlson23.github.io/birthday-app/',
     },
     {
-      image: 'src/images/my-queue.jpg',
+      image: './images/my-queue.jpg',
       description: 'My Queue is a full stack application for discovering new television shows to watch. It utilizes HTML, Tailwind CSS, Handlebars, Express, MySQL, GSAP, and is hosted on Heroku.',
       githubLink: 'https://github.com/Acolyte03/My-Queue',
       deployedLink: 'https://my-queue-proj2-17f6449399b4.herokuapp.com/',
     },
     {
-        image: 'src/images/fitness-tracker.png',
+        image: './images/fitness-tracker.png',
         description: 'My Fitness Tracker is a MERN full stack application for logging workouts. It utilizes MongoDB, Express, React, and Node.',
         githubLink: 'https://github.com/Bfinch3/fitness-tracker',
         deployedLink: 'Coming Soon',
       },
       {
-        image: 'src/images/password-generator.png',
+        image: './images/password-generator.png',
         description: 'Password Generator enables users to generate a random password based on their preferences. ',
         githubLink: 'https://github.com/clvalada/Password-Generator',
         deployedLink: 'https://clvalada.github.io/challenge-3-password-generator/',
       },
       {
-        image: 'src/images/sample-question.png',
+        image: './images/sample-question.png',
         description: 'Timed Coding Quiz is a 60-second multiple choice quiz about the fundamentals of JavaScript. It utilizes JavaScript, HTML, and CSS.',
         githubLink: 'https://github.com/clvalada/Timed-Coding-Quiz',
         deployedLink: 'https://clvalada.github.io/Timed-Coding-Quiz/',
       },
       {
-        image: 'src/images/note-taker.png',
+        image: './images/note-taker.png',
         description: 'Note Taker is an Express.js application that enables the user to take and save notes.',
         githubLink: 'https://github.com/clvalada/note-taker',
         deployedLink: 'https://note-taker-clv-f900bf932a65.herokuapp.com/public/index.html',
