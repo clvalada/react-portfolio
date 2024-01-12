0b584ea6654a67a9e93818af04badd1481fe46a9
clvalada <clvalada@gmail.com>
2024-01-12 14:33:31 -0500

    revising text

--- src/About.js
+++ src/About.js
@@ -12,11 +12,14 @@ const About = () => {
         </div>
         <div className="right-content">
           <p>
-            Full Stack Developer, life-long learner, and whiteboard enthusiast with unique customer-facing experience. 
+            Full Stack Developer, life-long learner, and whiteboard enthusiast with extensive customer-facing experience. 
             Over 15 years in Education Technology SaaS sales, customer success, and leadership roles.
            </p>  
            <p>
            I enjoy working with small, passionate teams where I can make a big impact.
+          </p>
+          <p>
+          HTML | CSS | Bootstrap | Tailwind | JavaScript | MySQL | Responsive Design | ReactJS | Handlebars.js | Node.js | Express | Heroku | MongoDB
           </p>
         </div>
       </div>
