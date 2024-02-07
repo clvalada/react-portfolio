8a01c827e2a5f1fa85094855f2bc8195356d7235
Chris Valada <145150623+clvalada@users.noreply.github.com>
2024-02-07 08:48:22 -0500

    completed js and css for the calendly link div

---
+++ src/Calendly.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Calendly = () => {
+    return (  
+        <div className="calendly">
+            <a href="https://calendly.com/clvalada/30-minute-consultation" target="_blank" rel="noopener noreferrer"><h1 className="calendly-h1">Click Here to Schedule a Free 30-Minute Consultation</h1></a>    
+            </div>
+    );
+}
+ 
+export default Calendly;
--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,16 @@ import About from './About';
 import Portfolio from './Portfolio';
 import Hero from './Hero';
 import FeaturedProjects from './FeaturedProjects';
+import Calendly from './Calendly';
 
 const Home = () => {
     return (  
         <div className="home">
             <div>
                 <Hero />
+            </div>
+            <div>
+                <Calendly />
             </div>
             <div>
                 <FeaturedProjects />
