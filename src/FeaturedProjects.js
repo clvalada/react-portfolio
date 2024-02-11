4a5ccf398fad94449bacd31e618ceb2e2fc5a89c
Chris Valada <145150623+clvalada@users.noreply.github.com>
2024-02-11 16:06:58 -0500

    adding .htaccess

--- src/FeaturedProjects.js
+++ src/FeaturedProjects.js
@@ -1,15 +1,16 @@
 import React from "react";
-import FeaturedImage from "./images/featured-vintage-drip.png"
+import FeaturedImage1 from "./images/featured-vintage-drip.png"
+import FeaturedImage2 from "./images/featured_flantastic.png"
 import Calendly from "./Calendly";
 
 const FeaturedProjects = () => {
     return ( 
         <div className="featured-projects">
-            <h1>Featured Project</h1>
+            <h1>Featured Projects</h1>
             <div className="featured-project-container">
                 <div className="featured-project-image">
                 <a href="https://vintagedrip.store" target="_blank" rel="noopener noreferrer">
-                        <img src={FeaturedImage} alt="Screenshots of VintageDrip.store on laptop and mobile" />
+                        <img src={FeaturedImage1} alt="Screenshots of VintageDrip.store on laptop and mobile" />
                 </a>
                 </div>
                 <div className="featured-project-description">
@@ -29,6 +30,31 @@ const FeaturedProjects = () => {
                         <a href="https://github.com/clvalada/vintagedrip" target="_blank" rel="noopener noreferrer">GitHub Repo </a>
                         <a href="https://vintagedrip.store" target="_blank" rel="noopener noreferrer">Deployed Project</a>
                     </div>
+                </div>
+            </div>
+            <div className="featured-project-container">
+                <div className="featured-project-description">
+                    <h2>¡Flantastic Treats!</h2>
+                    <br></br>
+                    <ul>
+                        <h3>¡Flantastic Treats! is an online storefront for a local baker of Cuban desserts. The site is a Single Page Application (SPA) and is fully responsive on mobile devices to ensure convenient user experience.</h3>
+                        <br></br>
+                        <h3>The site is a Single Page Application (SPA) and is fully responsive on mobile devices; prioritizing a fun and convenient user experience.</h3>
+                        <br></br>
+                        <h3>¡Flantastic Treats! is another example of my proficiency in front-end technologies and commitment to user-centric design.</h3>
+                    </ul>
+                    <br></br>
+                    <hr></hr>
+                    <br></br>
+                    <div className="featured-project-links">
+                        <a href="https://github.com/clvalada/flantastic" target="_blank" rel="noopener noreferrer">GitHub Repo </a>
+                        <a href="https://flantastic.netlify.com" target="_blank" rel="noopener noreferrer">Deployed Project</a>
+                    </div>
+                </div>
+                <div className="featured-project-image">
+                <a href="https://flantastic.netlify.com" target="_blank" rel="noopener noreferrer">
+                    <img src={FeaturedImage2} alt="Screenshots of Flantastic Treats on laptop and mobile" />
+                </a>
                 </div>
             </div>
             <Calendly />
